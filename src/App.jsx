0f98cb73e1e6a76159e1ba0f1972c3407b935d0f
Davit_Gelovani <davitgelovani@Davits-Air.netis.cc>
2023-04-02 22:19:39 +0400

    #6 - Added Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
+import React, { useRef } from 'react';
+
 import Nav from './components/Nav';
 import Jumbotron from './components/Jumbotron';
 import SoundSection from './components/SoundSection';
 import DisplaySection from './components/DisplaySection';
 import WebGiViewer from './components/WebgiViewer';
+import Loader from './components/Loader';
+
 function App() {
+  const webgiViewerRef = useRef();
+  const contentRef = useRef(null);
+
+  const handlePreview = () => {
+    webgiViewerRef.current.triggerPreview();
+  };
+
   return (
     <div className="App">
-      <Nav />
-      <Jumbotron />
-      <SoundSection />
-      <DisplaySection />
-      <WebGiViewer />
+      <Loader />
+      <div id="content" ref={contentRef}>
+        <Nav />
+        <Jumbotron />
+        <SoundSection />
+        <DisplaySection triggerPreview={handlePreview} />
+      </div>
+      <WebGiViewer contentRef={contentRef} ref={webgiViewerRef} />
     </div>
   );
 }
